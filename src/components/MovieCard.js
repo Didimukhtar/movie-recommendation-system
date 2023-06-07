9351288c3ab0b568bd60660939c9ce738e8fa54c
Okechukwu Chukwude <64587747+OkeyChukwude@users.noreply.github.com>
2023-06-07 13:27:29 +0100

    Fix default poster link

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,7 +10,7 @@ export default function MovieCard({movie}) {
     return (
         <Col className="d-flex align-items-stretch">
             <Card className="movie_card">
-                <Card.Img src={`${TOP_HUNDRED_BASEURL}${movie.poster_path}` || `${DEFAULT_POSTER}`}></Card.Img>
+                <Card.Img src={movie.poster_path ? `${TOP_HUNDRED_BASEURL}${movie.poster_path}` : `${DEFAULT_POSTER}`}></Card.Img>
                 <Card.Body>
                     <Card.Text className="movie_rating"><Badge pill bg="warning" text="dark">	&#9733; {movie.vote_average}</Badge></Card.Text>
                     <Card.Title className="fw-bolder">{movie.title}</Card.Title>
